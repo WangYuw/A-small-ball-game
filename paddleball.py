4657b237ae9ecbbdd85884f009e613dce2314992
wang_yuw <yuwei.wang.pluie@gmail.com>
2017-02-17 13:50:19 +0100

    create class paddle and game over if ball fall down the floor

--- paddleball.py
+++ paddleball.py
@@ -3,11 +3,13 @@ import random
 import time
 
 STEP = 3
+PA_STEP = 2
 #create class Ball
 class Ball:
     
-    def __init__(self, canvas, color):
+    def __init__(self, canvas, paddle, color):
         self.canvas = canvas
+        self.paddle = paddle
         self.id = canvas.create_oval(10, 10, 25, 25, fill=color)
         self.canvas.move(self.id, 245, 100)
         # starts = [-3, -2, -1, 1, 2, 3]
@@ -17,6 +19,7 @@ class Ball:
         self.y = STEP
         self.canvas_height = self.canvas.winfo_height() # get the height of current window
         self.canvas_width = self.canvas.winfo_width() # get the width of current window
+        self.hit_bottom = False
 
     def draw(self):
         self.canvas.move(self.id, self.x, self.y)
@@ -24,12 +27,46 @@ class Ball:
         if pos[1] <= 0:
             self.y = STEP
         if pos[3] >= self.canvas_height:
-            self.y = -STEP
+            self.hit_bottom = True
+        if self.hit_paddle(pos) == True:
+            self.y = -STEP # change direction
         if pos[0] <= 0:
             self.x = STEP
         if pos[2] >= self.canvas_width:
             self.x = -STEP
 
+    def hit_paddle(self, pos):
+        paddle_pos = self.canvas.coords(self.paddle.id)
+        if pos[2] >= paddle_pos[0] and pos[0] <= paddle_pos[2]:
+            if pos[3] >= paddle_pos[1] and pos[3] <= paddle_pos[3]:
+                return True
+        return False
+
+# create class Paddle
+class Paddle:
+    
+    def __init__(self, canvas, color):
+        self.canvas = canvas
+        self.id = canvas.create_rectangle(0, 0, 100, 10, fill=color)
+        self.canvas.move(self.id, 200, 350)
+        self.x = 0
+        self.canvas_width = self.canvas.winfo_width()
+        self.canvas.bind_all('<KeyPress-Left>', self.turn_left)
+        self.canvas.bind_all('<KeyPress-Right>', self.turn_right)
+
+    def draw(self):
+        self.canvas.move(self.id, self.x, 0)
+        pos = self.canvas.coords(self.id)
+        if pos[0] <= 0:
+            self.x = 0
+        elif pos[2] >= self.canvas_width:
+            self.x = 0
+
+    def turn_left(self, evt):
+        self.x = -PA_STEP
+
+    def turn_right(self, evt):
+        self.x = PA_STEP
 
 # Create a canvas
 tk = Tk()
@@ -40,10 +77,13 @@ canvas = Canvas(tk,width=500,height=400,bd=0,highlightthickness=0) # no edge
 canvas.pack()
 tk.update() # preparer initialision
 
-ball = Ball(canvas, 'red')
+paddle = Paddle(canvas, 'blue')
+ball = Ball(canvas, paddle, 'red')
 # main loop
 while 1:
-    ball.draw()
+    if ball.hit_bottom == False:
+        ball.draw()
+        paddle.draw()
     tk.update_idletasks() # redraw the canvas
     tk.update()
     time.sleep(0.01)       
